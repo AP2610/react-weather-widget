843795d34a9108c71b5d4662e5a430830fe8be16
Arjun Puri <arjun_261094@hotmail.com>
2020-01-09 20:25:35 +0100

    Adds Moment package to format date

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./styles.css";
 import Header from "./Components/Header";
 import WeatherContainer from "./Components/WeatherContainer";
 
-console.log(process.env.REACT_APP_API_KEY)
-
 class App extends Component {
   render() {
     return (
--- src/Components/WeatherContainer.js
+++ src/Components/WeatherContainer.js
@@ -1,33 +1,53 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "../styles.css";
 
 const key = "78ddbb0eef728d7c015c7f1e66d4a626"
-const {lat, long} = {lat: 52.370216, long: 4.895168}
+const { lat, long } = { lat: 52.370216, long: 4.895168 }
 const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${key}/${lat},${long}`
 
 class WeatherContainer extends Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            data: {}
+            isLoaded: false,
+            timeZone: "",
+            CurrentData: {}
         }
     };
 
     componentDidMount = () => {
         fetch(url)
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-            this.setState({data: response})
-        })
+            .then(response => response.json())
+            .then(response => {
+                // console.log("Response: ",response)
+                this.setState({
+                    isLoaded: true,
+                    timeZone: response.timezone,
+                    CurrentData: response.currently 
+                })
+            })
+    }
+
+    dateFormatter = () => {
+        const days = ["Monday","Tuesday","Wednesday", "Thursday", "Friday"]
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        if (this.state.isLoaded) {
+            const UnixTime = this.state.CurrentData.time
+            const date = new Date(UnixTime*1000)
+            const day = days[date.getUTCDay()]
+            const month = months[date.getUTCMonth()]
+            console.log(day, month)
+        };
     }
 
     render() {
-        console.log("Key: ", process.env.API_KEY)
         console.log("State: ", this.state)
+        this.dateFormatter()
         return (
             <div className="weather-container">
                 <h2>Weather Forecast</h2>
+                <h3>{this.state.timezone}</h3>
+                <h4>{}</h4>
             </div>
         );
     };
